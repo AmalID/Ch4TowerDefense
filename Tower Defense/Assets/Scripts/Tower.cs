685c993834e9dad898bc81fd0e0e2541b75c11e8
AmalID <amalianeko21@gmail.com>
2021-05-04 23:36:34 +0700

    -

---
+++ Tower Defense/Assets/Scripts/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _towerPlace;
+    [SerializeField] private SpriteRenderer _towerHead;
+
+    [SerializeField] private int shootPower = 1;
+    [SerializeField] private float _shootDistance = 1f;
+    [SerializeField] private float _shootDelay = 5f;
+    [SerializeField] private float _bulletSpeed = 1f;
+    [SerializeField] private float _bulletSplashRadius = 0f;
+
+    public Sprite GetHeadIcon()
+    {
+        return _towerHead.sprite;
+    }
+}
